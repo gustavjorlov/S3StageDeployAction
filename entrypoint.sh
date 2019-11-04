61096c96437ec80830181145d1fbaefbf22b89d1
Gustav Jorlöv <gustav.jorlov@gmail.com>
2019-11-04 21:36:28 +0100

    [id-23123] more config again

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ export AWS_DEFAULT_REGION="eu-west-1"
 
 aws s3 ls
 
-aws.sh s3 cp $GITHUB_WORKSPACE s3://frontfailure/build
+aws s3 cp $GITHUB_WORKSPACE s3://frontfailure/build
 
 echo ::set-output name=s3_url::"s3://frontfailure"
