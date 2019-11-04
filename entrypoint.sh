7e8c7a8128d659025303b95fc73d92dda903ee42
Gustav Jorlöv <gustav.jorlov@gmail.com>
2019-11-04 21:14:56 +0100

    [id-123] updated action hash again again

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ export AWS_ACCESS_KEY_ID=$3
 export AWS_SECRET_ACCESS_KEY=$2
 export AWS_DEFAULT_REGION="eu-west-1"
 
-./aws s3 ls
+aws s3 ls
 
-./aws.sh s3 cp ./package.json s3://frontfailure
+aws.sh s3 cp ./package.json s3://frontfailure
 
 echo ::set-output name=s3_url::"s3://frontfailure"
