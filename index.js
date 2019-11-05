fde15e06ca53c58583796317c487d7d880830aa2
Gustav Jorlöv <gustav.jorlov@gmail.com>
2019-11-05 11:45:21 +0100

    [id-231] changed action

--- index.js
+++ index.js
@@ -44,11 +44,11 @@ const run = async () => {
     await exec.exec("/usr/local/bin/aws", ["s3", "ls"], awsOptions);
     await exec.exec(
       "/usr/local/bin/aws",
-      ["s3", "cp", "build", "s3://" + s3Bucket],
+      ["s3", "cp", "build", "s3://" + s3Bucket + "/", "--recursive"],
       awsOptions
     );
 
-    console.log("done", { output,payload });
+    console.log("done", { output, payload });
 
     core.setOutput("s3_url", "the://url");
   } catch (error) {
