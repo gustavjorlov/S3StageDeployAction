5c885a13fcdd0b518637b42c3ecfe5c537ecab68
Gustav Jorlöv <gustav.jorlov@gmail.com>
2019-11-05 23:58:42 +0100

    Updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/sh -l
 
-echo "Hello $1 $2 $3"
-
 export AWS_ACCESS_KEY_ID=$3
 export AWS_SECRET_ACCESS_KEY=$2
 export AWS_DEFAULT_REGION="eu-west-1"
 
-ls
-
 aws s3 cp build "s3://${1}/docker/" --recursive
 
-echo ::set-output name=s3_url::"s3://frontfailure"
+echo ::set-output name=s3_url::"s3://${1}/docker/"
