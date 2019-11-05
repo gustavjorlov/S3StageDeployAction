923a8a23f9ec60e4cce428782a8b97430ed45dee
Gustav Jorlöv <gustav.jorlov@gmail.com>
2019-11-05 10:38:20 +0100

    [id-231] changed action

--- index.js
+++ index.js
@@ -4,39 +4,36 @@ const exec = require("@actions/exec");
 
 const AWS = require("aws-sdk");
 
-try {
-  const s3Bucket = core.getInput("s3-bucket");
-  const secretAccessKey = core.getInput("secret-access-key");
-  const accessKeyId = core.getInput("access-key-id");
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-
-  let output = "";
-
-  await exec.exec(
-    `AWS_ACCESS_KEY_ID=${accessKeyId} AWS_SECRET_ACCESS_KEY=${secretAccessKey} aws s3 --region=eu-west-1 ls index.js`,
-    {
-      listeners: {
-        stdout: data => {
-          output += data.toString();
-        },
-        stderr: data => {
-          output += data.toString();
+const run = async = () => {
+  try {
+    const s3Bucket = core.getInput("s3-bucket");
+    const secretAccessKey = core.getInput("secret-access-key");
+    const accessKeyId = core.getInput("access-key-id");
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+  
+    let output = "";
+  
+    await exec.exec(
+      `AWS_ACCESS_KEY_ID=${accessKeyId} AWS_SECRET_ACCESS_KEY=${secretAccessKey} aws s3 --region=eu-west-1 ls index.js`,
+      {
+        listeners: {
+          stdout: data => {
+            output += data.toString();
+          },
+          stderr: data => {
+            output += data.toString();
+          }
         }
       }
-    }
-  );
-
-  // const S3 = new AWS.S3({
-  //   accessKeyId: accessKeyId,
-  //   secretAccessKey: secretAccessKey
-  // });
-
-  // console.log({ s3Bucket, secretAccessKey, accessKeyId });
-  // console.log(`The event payload: ${payload}`);
-
-  console.log("done", { output });
-
-  core.setOutput("s3_url", "the://url");
-} catch (error) {
-  core.setFailed(error.message);
+    );
+  
+    console.log("done", { output });
+  
+    core.setOutput("s3_url", "the://url");
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+  
 }
+
+run()
