247347abfe8ba006c7f68dfbdaec2648dcafdbd4
Gustav Jorlöv <gustav.jorlov@gmail.com>
2019-11-05 10:34:09 +0100

    Exec stuff

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const exec = require("@actions/exec");
+
 const AWS = require("aws-sdk");
 
 try {
@@ -8,13 +10,31 @@ try {
   const accessKeyId = core.getInput("access-key-id");
   const payload = JSON.stringify(github.context.payload, undefined, 2);
 
-  const S3 = new AWS.S3({
-    accessKeyId: accessKeyId,
-    secretAccessKey: secretAccessKey
-  });
+  let output = "";
 
-  console.log({ s3Bucket, secretAccessKey, accessKeyId });
-  console.log(`The event payload: ${payload}`);
+  await exec.exec(
+    `AWS_ACCESS_KEY_ID=${accessKeyId} AWS_SECRET_ACCESS_KEY=${secretAccessKey} aws s3 --region=eu-west-1 ls index.js`,
+    {
+      listeners: {
+        stdout: data => {
+          output += data.toString();
+        },
+        stderr: data => {
+          output += data.toString();
+        }
+      }
+    }
+  );
+
+  // const S3 = new AWS.S3({
+  //   accessKeyId: accessKeyId,
+  //   secretAccessKey: secretAccessKey
+  // });
+
+  // console.log({ s3Bucket, secretAccessKey, accessKeyId });
+  // console.log(`The event payload: ${payload}`);
+
+  console.log("done", { output });
 
   core.setOutput("s3_url", "the://url");
 } catch (error) {
