7f43781f26d7ad2d3974640b2a3a0c4100f6cba9
Gustav Jorlöv <gustav.jorlov@gmail.com>
2019-11-05 10:51:58 +0100

    [id-231] changed action

--- index.js
+++ index.js
@@ -10,30 +10,28 @@ const run = async () => {
     const secretAccessKey = core.getInput("secret-access-key");
     const accessKeyId = core.getInput("access-key-id");
     const payload = JSON.stringify(github.context.payload, undefined, 2);
-  
+
     let output = "";
-  
-    await exec.exec(
-      `AWS_ACCESS_KEY_ID=${accessKeyId} AWS_SECRET_ACCESS_KEY=${secretAccessKey} aws s3 --region=eu-west-1 ls index.js`,
-      {
-        listeners: {
-          stdout: data => {
-            output += data.toString();
-          },
-          stderr: data => {
-            output += data.toString();
-          }
+    await exec.exec(`export AWS_ACCESS_KEY_ID=${accessKeyId}`);
+    await exec.exec(`export AWS_SECRET_ACCESS_KEY=${secretAccessKey}`);
+    await exec.exec(`export AWS_DEFAULT_REGION=eu-west-1`);
+    await exec.exec(`aws s3 ls`, {
+      listeners: {
+        stdout: data => {
+          output += data.toString();
+        },
+        stderr: data => {
+          output += data.toString();
         }
       }
-    );
-  
+    });
+
     console.log("done", { output });
-  
+
     core.setOutput("s3_url", "the://url");
   } catch (error) {
     core.setFailed(error.message);
   }
-  
-}
+};
 
-run()
+run();
