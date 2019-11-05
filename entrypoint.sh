7dd2e2b268e5ae98c3d9d6bf9a6998eb3b8402f6
Gustav Jorlöv <gustav.jorlov@gmail.com>
2019-11-05 19:04:21 +0100

    Switch back to Docker 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,16 +6,8 @@ export AWS_ACCESS_KEY_ID=$3
 export AWS_SECRET_ACCESS_KEY=$2
 export AWS_DEFAULT_REGION="eu-west-1"
 
-aws s3 ls
-
 ls
 
-ls / 
-
-ls ~/
-
-ls $GITHUB_WORKSPACE
-
-# aws s3 cp $GITHUB_WORKSPACE s3://frontfailure/build
+aws s3 cp build "s3://${1}/docker/"
 
 echo ::set-output name=s3_url::"s3://frontfailure"
